85813901b4b16d44bedb8178652c3b4d87cf5b02
Ilmārs <darkswormlv@yahoo.com>
2016-11-14 01:56:21 +0200

    strict and non-blocking error handling, red error output :palm_tree:

--- beautify.js
+++ beautify.js
@@ -5,32 +5,50 @@ var beautify_js = require('js-beautify').js_beautify,
     fs = require('fs'),
     process = require('process');
 
-var files = process.argv.slice(2), i;
+var files = process.argv.slice(2), i, errors = [];
 
 for (i in files) {
-    let file = files[i];
+    let data,
+        beautify,
+        file = files[i];
 
-    fs.readFile(file, 'utf8', function (err, data) {
-        if (err) {
-            throw err;
+    try {
+        data = fs.readFileSync(file, 'utf-8');
+    } catch (err) {
+        errors.push(file + "::" + err.stack);
+        continue;
+    }
+
+    if (file.endsWith('.css')) {
+        beautify = beautify_css;
+    } else if (file.endsWith('.js')) {
+        beautify = beautify_js;
+    } else if (file.endsWith('.html')) {
+        beautify = beautify_html;
+    }
+
+    if (beautify) {
+        try {
+            fs.writeFileSync(file, beautify(data, {indent_size: 4}));
+        } catch (err) {
+            errors.push(file + "::" + err.stack);
         }
+    }
+}
 
-        let beautify;
+if(errors.length){
+    var date = new Date().toISOString()
+        .replace(/T/, ' ')
+        .replace(/\..+/, '');
+    var text = '';
 
-        if (file.endsWith('.css')) {
-            beautify = beautify_css;
-        } else if (file.endsWith('.js')) {
-            beautify = beautify_js;
-        } else if (file.endsWith('.html')) {
-            beautify = beautify_html;
-        }
+    for(i in errors) {
+        text += date + ' :: ' + errors[i] + '\n';
+    }
 
-        if (beautify) {
-            fs.writeFile(file, beautify(data, {indent_size: 4}), function (err) {
-                if (err) {
-                    throw err;
-                }
-            })
-        }
+    fs.appendFile('error.log', text.slice(0, -1), function(){
+        process.exit(1);
     });
+} else {
+    process.exit(0);
 }
--- run.sh
+++ run.sh
@@ -7,11 +7,24 @@ function finish {
     rm ${lockfile}
     # if failed reset assets folder to HEAD
     if [ "$1" -eq "1" ]; then
-        git rm -rf assets/*
         git checkout HEAD -- assets/
     fi
     exit $1
 }
+
+function redoutput {
+    echo -e '\033[0;31m'$1'\033[0m'
+}
+
+function error {
+    redoutput $1
+    echo $(date +"%r %d %h %y") ":" $1 | tee -a error.log
+}
+
+if [ $(whoami) == "root" ]; then
+    echo "Please don't run this as root"
+    finish 1
+fi
 
 if [ -f "$lockfile" ] && kill -0 "$(cat ${lockfile})" 2>/dev/null; then
     echo Still running
@@ -20,14 +33,21 @@ fi
 
 echo $$ > ${lockfile}
 
-# remove old files
-git rm -rf ${PWD}/assets/*
-
 # get array of relative dirs and urls
 result=($(node getassetlist.js | sed 's/\"//g' | sed 's/,/ /g' | tr -d '[],'))
 
+if [[ -z "${result}" ]]; then
+    error "failed to parse urls.json"
+    finish 1
+fi
+
+# remove old files
+rm -rf assets/*
+
 len=$(expr ${#result[@]} / 2)
+# parameter for beautify script, all downloaded file absolute paths with spaces as delimiters
 files=""
+exit_code=0
 
 echo
 
@@ -36,19 +56,24 @@ do
     # make file directory recursively
     mkdir -p ${PWD}/assets/${result[$i]%/*}
     # wget file and save to defined directory
-    wget -O ${PWD}/assets/${result[$i]} ${result[$(expr ${i} + ${len})]} -q --show-progress --no-cache || { echo wget failed; finish 1; }
+    wget -O ${PWD}/assets/${result[$i]} ${result[$(expr ${i} + ${len})]} -q --show-progress --no-cache 2>&1
+    if [ "$?" != "0" ]; then
+        error "wget failed for url: "${result[$(expr ${i} + ${len})]}
+        exit_code=1
+    fi
     files+=${PWD}/assets/${result[$i]}" "
 done
 
-echo
+node beautify.js ${files:0:${#files}-1}
+beautify_result=$?
 
-output=$(node beautify.js ${files:0:${#files}-1} 2>&1)
-
-if [[ ! -z $output ]]; then
-    echo "beautify.js failed, view error.log for details"
-    echo ${output} >> error.log
-    finish 1
+if [[ "$beautify_result" == "1" ]]; then
+    echo
+    redoutput "Beautify returned errors, check error.log"
+    exit_code=1
 fi
+
+echo
 
 git add -A
 
@@ -57,7 +82,7 @@ if [[ $(git status --porcelain) ]]; then
     git commit -m "$(git status --porcelain | sed ':a;N;$!ba;s/\n/, /g')"
     git push
 else
-    echo No changes found
+    echo "No changes found"
 fi
 
-finish 0
+finish ${exit_code}
